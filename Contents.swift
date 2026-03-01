e2b47f01367d5c9ab2b7d91973ab54aee16f4cb1
Roi Mark Gamba <gambaroimark@gmail.com>
2026-03-01 19:39:50 +0800

    initial working shunting yard script and while loop refactor

--- Contents.swift
+++ Contents.swift
@@ -1,3 +1,7 @@
+
+/**
+ Function for infix, preparation for postfix computation
+ */
 func precedence(of op: Character) -> Int {
     switch op {
         case "+", "-": return 1
@@ -7,47 +11,90 @@ func precedence(of op: Character) -> Int {
 }
 
 var operatorStack: [Character] = []
-var infixQueue: [Character] = []
-var operation: String = "5*4+3"
+var infixQueue: [String] = []
+var operation: String = "52*4+3-21"
+var numberSeriesHolder: String = ""
+var pushToQueue: Bool = false
 
-for charT in operation {
-    if(charT.isNumber){
-        infixQueue.append(charT)
-    } else {
-        if charT == "(" {
-            operatorStack.append(charT)
-        }
+var index = operation.startIndex
+
+while index < operation.endIndex {
+    let charHolder = operation[index]
+    
+    if(charHolder.isNumber){
+        let nextCheck = operation.index(after: index)
         
-        if charT == ")" {
-            while let last = operatorStack.last, last != "(" {
-                infixQueue.append(operatorStack.popLast()!)
-            }
-
-            if operatorStack.last == "(" {
-                operatorStack.popLast()
-            }
-            
-        }
-        
-
-        if charT != "(" && charT != ")" {
-            while operatorStack.isEmpty == false
-                   && operatorStack.last != nil
-                   && operatorStack.last != "("
-                   && operatorStack.last != ")"
-                   && precedence(of: charT) <= precedence(of: operatorStack.last!)
-            {
-                infixQueue.append(operatorStack.popLast()!)
-                
-            }
-            operatorStack.append(charT)
-            
-        }
-
     }
+    
+    index = operation.index(after: index)
+
 }
+//for charT in operation {
+//    if(charT.isNumber){
+//        numberSeriesHolder = !infixQueue.isEmpty ? infixQueue.removeLast() : ""
+//        print(numberSeriesHolder)
+//        numberSeriesHolder.append(charT)
+//        
+//        infixQueue.append(numberSeriesHolder)
+//    } else {
+//        numberSeriesHolder = ""
+//        print(numberSeriesHolder)
+//        
+//        if charT == "(" {
+//            operatorStack.append(charT)
+//        }
+//        
+//        if charT == ")" {
+//            while let last = operatorStack.last, last != "(" {
+//                infixQueue.append(String(operatorStack.popLast()!))
+//            }
+//
+//            if operatorStack.last == "(" {
+//                operatorStack.popLast()
+//            }
+//            
+//        }
+//        
+//        if charT != "(" && charT != ")" {
+//            while operatorStack.isEmpty == false
+//                   && operatorStack.last != nil
+//                   && operatorStack.last != "("
+//                   && operatorStack.last != ")"
+//                   && precedence(of: charT) <= precedence(of: operatorStack.last!)
+//            {
+//                infixQueue.append(String(operatorStack.popLast()!))
+//                
+//            }
+//            operatorStack.append(charT)
+//            
+//        }
+//    }
+//}
 
-let finalOperator = operatorStack.popLast()
-infixQueue.append(finalOperator!)
+//let finalOperator = operatorStack.popLast()
+//infixQueue.append(String(finalOperator!))
+//
+////print(operatorStack)
+//print(infixQueue)
 
-print(infixQueue)
+
+/**
+ Postfix computation
+ */
+
+//var postfixStack: [Character] = []
+//var right: String = ""
+//var left: String = ""
+//var op: Character = " "
+//
+//for charT in infixQueue {
+//    right = ""
+//    left = ""
+//    op = " "
+//    
+//    if charT.isNumber {
+//        postfixStack.append(charT)
+//    } else {
+//        
+//    }
+//}
